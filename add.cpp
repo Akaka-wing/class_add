14bdd9056f180707eb12592b934d568394db10a1
Akaka <1025281520@qq.com>
2022-02-07 10:33:06 +0800

    first commit

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include<add.h>
+
+add::add(double a, double b)
+{
+
+}
+
+add::~add()
+{
+}
+
+add::solution()
+{
+}
---
+++ add.h
@@ -0,0 +1,10 @@
+class add
+{
+public:
+    add(double a, double b);
+    ~add();
+    solution();
+
+private:
+    double n, m;
+}
